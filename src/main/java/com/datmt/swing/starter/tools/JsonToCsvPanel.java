d488d4595c95a68e324506d3d40e40b3773dbab2
damtmt <dat.tm24@gmail.com>
2025-01-11 11:06:31 +0700

    clean up code

--- src/main/java/com/datmt/swing/starter/tools/JsonToCsvPanel.java
+++ src/main/java/com/datmt/swing/starter/tools/JsonToCsvPanel.java
@@ -1,16 +1,7 @@
 package com.datmt.swing.starter.tools;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.io.File;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
 
 public class JsonToCsvPanel extends JPanel {
 
